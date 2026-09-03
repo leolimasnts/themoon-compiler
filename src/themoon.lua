dc2eaad30a3f5cb38184e5239235540bfd4c1bcb
Leo <leolimaasantos@gmail.com>
2026-09-03 13:58:48 -0300

    implement peek mechanic and print in the correct format

--- src/themoon.lua
+++ src/themoon.lua
@@ -26,142 +26,149 @@ The Moon (From Soul Eater)
 -- LEXER
 -- The Lexer Job is to read the code as a raw String and break it into tokens. A token is a word that the Parser knows for sure
 
+local inspect = require("inspect")
+
 Lexer = {}
 
-
 Lexer.init_lexer = function()
-  Lexer.buffer = io.read("*all")
-  Lexer.curr_position = 0
-  Lexer.curr_char = nil
-  Lexer.curr_line = 0
-  Lexer.curr_column = 0
-  Lexer.curr_token = nil
+	Lexer.buffer = io.read("*all")
+	Lexer.curr_position = 0
+	Lexer.curr_char = nil
+	Lexer.next_char = nil
+	Lexer.curr_line = 0
+	Lexer.curr_column = 0
+	Lexer.token_column = 0
+	Lexer.token_line = 0
+	Lexer.curr_token = nil
+	Lexer.should_stop = false
+	Lexer.move_foward()
 end
 
-
-Lexer.move_foward = function()
-  Lexer.curr_position = Lexer.curr_position + 1
-  Lexer.curr_char = string.sub(Lexer.buffer, Lexer.curr_position, Lexer.curr_position)
+Lexer.move_foward = function(i)
+	i = i or 1
+	Lexer.curr_position = Lexer.curr_position + i
+	Lexer.curr_char = string.sub(Lexer.buffer, Lexer.curr_position, Lexer.curr_position)
+	Lexer.curr_column = Lexer.curr_column + i
 end
 
-Lexer.move_backward = function()
-  Lexer.curr_position = Lexer.curr_position - 1
-  Lexer.curr_char = string.sub(Lexer.buffer, Lexer.curr_position, Lexer.curr_position)
+Lexer.peek_next = function(i)
+	i = i or 1
+	return string.sub(Lexer.buffer, Lexer.curr_position + i, Lexer.curr_position + i)
 end
 
-Token = function(key, val) return { key, val } end
-
---[[
-if Lexer.curr_char == "+" or "-" or "*" or "/" or "%" or "^" or "#" or ";" or ":" or "," or "(" or ")" or "[" or "]" or "{" or "}" then
-  Lexer.curr_token = Token(Lexer.curr_char)
-end
---]]
-
-Lexer.get_next_token = function()
-  if Lexer.curr_char == "" then
-    Lexer.curr_token = Token("EOF")
-    -- single char tokens
-
-
-
-
-  elseif Lexer.curr_char == "+" then
-    Lexer.curr_token = Token("+") -- +
-  elseif Lexer.curr_char == "-" then
-    Lexer.curr_token = Token("-") -- -
-  elseif Lexer.curr_char == "*" then
-    Lexer.curr_token = Token("*") -- *
-  elseif Lexer.curr_char == "/" then
-    Lexer.curr_token = Token("/") -- /
-  elseif Lexer.curr_char == "%" then
-    Lexer.curr_token = Token("%") -- %
-  elseif Lexer.curr_char == "^" then
-    Lexer.curr_token = Token("^") -- ^
-  elseif Lexer.curr_char == "#" then
-    Lexer.curr_token = Token("#") -- #
-  elseif Lexer.curr_char == ";" then
-    Lexer.curr_token = Token(";") -- ;
-  elseif Lexer.curr_char == ":" then
-    Lexer.curr_token = Token(":") -- :
-  elseif Lexer.curr_char == "," then
-    Lexer.curr_token = Token(",") -- ,
-  elseif Lexer.curr_char == "(" then
-    Lexer.curr_token = Token("(") -- (
-  elseif Lexer.curr_char == ")" then
-    Lexer.curr_token = Token(")") -- )
-  elseif Lexer.curr_char == "[" then
-    Lexer.curr_token = Token("[") -- [
-  elseif Lexer.curr_char == "]" then
-    Lexer.curr_token = Token("]") -- ]
-  elseif Lexer.curr_char == "{" then
-    Lexer.curr_token = Token("{") -- {
-  elseif Lexer.curr_char == "}" then
-    Lexer.curr_token = Token("}") -- }
-
-
-
-
-
-  elseif Lexer.curr_char == "=" then
-    Lexer.move_foward()
-    if Lexer.curr_char == "=" then
-      Lexer.curr_token = Token("==") -- ==
-    else
-      Lexer.move_backward()
-      Lexer.curr_token = Token("=") -- =
-    end
-  elseif Lexer.curr_char == "~" then
-    Lexer.move_foward()
-    if Lexer.curr_char == "=" then
-      Lexer.curr_token = Token("~=") -- ~=
-    else
-      print("Error: could not identify char \"", Lexer.curr_char, "\"")
-    end
-  elseif Lexer.curr_char == "<" then
-    Lexer.move_foward()
-    if Lexer.curr_char == "=" then
-      Lexer.curr_token = Token("<=") -- <=
-    else
-      Lexer.move_backward()
-      Lexer.curr_token = Token("<") -- <
-    end
-  elseif Lexer.curr_char == ">" then
-    Lexer.move_foward()
-    if Lexer.curr_char == "=" then
-      Lexer.curr_token = Token(">=") -- >=
-    else
-      Lexer.move_backward()
-      Lexer.curr_token = Token(">") --  >
-    end
-  elseif Lexer.curr_char == "." then
-    Lexer.move_foward()
-    if Lexer.curr_char == "." then
-      Lexer.move_foward()
-      if Lexer.curr_char == "." then
-        Lexer.curr_token = Token("...") -- ...
-      else
-        Lexer.move_backward()
-        Lexer.curr_token = Token("..") -- ..
-      end
-    else
-      Lexer.move_backward()
-      Lexer.curr_token = Token(".") -- .
-    end
-  else
-    print("its not a symbol..")
-  end
-
--- 
-
-
-  Lexer.move_foward()
-  return Lexer.curr_token
+Token = function(key, val)
+	return { key, val }
 end
 
+Lexer.tokenize = function()
+	Lexer.curr_token = Token("ERR")
+	Lexer.token_column = Lexer.curr_column
+	Lexer.token_line = Lexer.curr_line
 
+	if Lexer.curr_char == "" then
+		Lexer.curr_token = Token("EOF")
+		Lexer.should_stop = true
+
+	-- single char tokens
+	elseif Lexer.curr_char == "+" then
+		Lexer.curr_token = Token("+") -- +
+	elseif Lexer.curr_char == "-" then
+		Lexer.curr_token = Token("-") -- -
+	elseif Lexer.curr_char == "*" then
+		Lexer.curr_token = Token("*") -- *
+	elseif Lexer.curr_char == "/" then
+		Lexer.curr_token = Token("/") -- /
+	elseif Lexer.curr_char == "%" then
+		Lexer.curr_token = Token("%") -- %
+	elseif Lexer.curr_char == "^" then
+		Lexer.curr_token = Token("^") -- ^
+	elseif Lexer.curr_char == "#" then
+		Lexer.curr_token = Token("#") -- #
+	elseif Lexer.curr_char == ";" then
+		Lexer.curr_token = Token(";") -- ;
+	elseif Lexer.curr_char == ":" then
+		Lexer.curr_token = Token(":") -- :
+	elseif Lexer.curr_char == "," then
+		Lexer.curr_token = Token(",") -- ,
+	elseif Lexer.curr_char == "(" then
+		Lexer.curr_token = Token("(") -- (
+	elseif Lexer.curr_char == ")" then
+		Lexer.curr_token = Token(")") -- )
+	elseif Lexer.curr_char == "[" then
+		Lexer.curr_token = Token("[") -- [
+	elseif Lexer.curr_char == "]" then
+		Lexer.curr_token = Token("]") -- ]
+	elseif Lexer.curr_char == "{" then
+		Lexer.curr_token = Token("{") -- {
+	elseif Lexer.curr_char == "}" then
+		Lexer.curr_token = Token("}") -- }
+
+	-- multichar tokens
+	elseif Lexer.curr_char == "=" then
+		if Lexer.peek_next() == "=" then
+			Lexer.move_foward()
+			Lexer.curr_token = Token("==") -- ==
+		else
+			Lexer.curr_token = Token("=") -- =
+		end
+	elseif Lexer.curr_char == "~" then
+		if Lexer.peek_next() == "=" then
+			Lexer.move_foward()
+			Lexer.curr_token = Token("~=") -- ~=
+		else
+			Lexer.curr_token = Token("ERR")
+			Lexer.should_stop = true
+		end
+	elseif Lexer.curr_char == "<" then
+		if Lexer.peek_next() == "=" then
+			Lexer.move_foward()
+			Lexer.curr_token = Token("<=") -- <=
+		else
+			Lexer.curr_token = Token("<") -- <
+		end
+	elseif Lexer.curr_char == ">" then
+		if Lexer.peek_next() == "=" then
+			Lexer.move_foward()
+			Lexer.curr_token = Token(">=") -- >=
+		else
+			Lexer.curr_token = Token(">") --  >
+		end
+	elseif Lexer.curr_char == "." then
+		if Lexer.peek_next() == "." then
+			if Lexer.peek_next(2) == "." then
+				Lexer.move_foward(2)
+				Lexer.curr_token = Token("...") -- ...
+			else
+				Lexer.move_foward()
+				Lexer.curr_token = Token("..") -- ..
+			end
+		else
+			Lexer.curr_token = Token(".") -- .
+		end
+	elseif Lexer.curr_char == " " then
+		Lexer.move_foward()
+		Lexer.tokenize()
+	elseif Lexer.curr_char == "\n" then
+		Lexer.curr_line = Lexer.curr_line + 1
+		Lexer.curr_column = 0
+		Lexer.move_foward()
+		Lexer.tokenize()
+	else
+		print('its not a symbol.. Im reading "', Lexer.curr_char, '"')
+		Lexer.curr_token = Token("ERR")
+		Lexer.should_stop = true
+	end
+
+	return Lexer.curr_token
+end
 
 -- running the lexer
 
 Lexer.init_lexer()
 
-print(Lexer.buffer)
+print("String:", Lexer.buffer)
+while not Lexer.should_stop do
+	Lexer.tokenize()
+	print(Lexer.token_line, "\t", Lexer.token_column, "\t", next(Lexer.curr_token))
+	Lexer.move_foward()
+end
